87eb24cc63ad37d8f86f10ea563c8a6dd2764e2c
Siddesh Pillai <siddeshvijji@gmail.com>
2018-11-17 13:11:07 -0800

    Added kotlin source files

---
+++ app/src/main/java/com/udacity/notepad/data/DataStore.kt
@@ -0,0 +1,29 @@
+package com.udacity.notepad.data
+
+import android.content.Context
+import org.jetbrains.anko.doAsync
+
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+object DataStore {
+
+//    val EXEC: Executor = Executors.newSingleThreadExecutor()
+
+    @JvmStatic
+    lateinit var notes: NoteDatabase
+        private set
+
+    fun init(context: Context) {
+        notes = NoteDatabase(context)
+    }
+
+    fun execute(runnable: Runnable) {
+//        EXEC.execute(runnable)
+        doAsync { runnable.run()  }
+    }
+
+    fun execute(fn: () -> Unit) {
+        doAsync { fn() }
+    }
+}
